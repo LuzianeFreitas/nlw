4bd5e7f7b13c6e0e5423e537d88baa4b4ec98ba3
LuzianeFreitas <freitas.lu@outlook.com>
2020-08-07 21:35:19 -0300

    Finished bd

--- src/pages.js
+++ src/pages.js
@@ -35,6 +35,10 @@ async function pageStudy(req, res) {
         const db = await dataBase
         const proffys = await db.all(query)
 
+        proffys.map((proffy) => {
+            proffy.subject = getSubject(proffy.subject)
+        })
+
         return res.render('study.html', { proffys, subjects, filters, weekdays })
     } catch (error) {
         console.log(error);
@@ -43,20 +47,52 @@ async function pageStudy(req, res) {
 }
 
 function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-        return res.redirect("/study")
-    }
+    
     
     return res.render("give-classes.html", { subjects, weekdays })
+}
+
+async function saveClasses(req, res) {
+    const createProffy = require('./database/createProffy')
+
+    const proffyValue = {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio: req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost,
+    }
+
+    const classScheduleValues = req.body.weekday.map((weekday, index) => {
+        return {
+            weekday: weekday,
+            time_from: convertHoursToMinutes(req.body.time_from[index]),
+            time_to: convertHoursToMinutes(req.body.time_to[index])
+        }
+    })
+
+    try {
+        const db = await dataBase
+        await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 module.exports = {
     pageLanding,
     pageStudy,
-    pageGiveClasses
+    pageGiveClasses,
+    saveClasses
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const server = express()
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 // Importando o nunjucks - para renderização das paginas
 const nunjucks = require('nunjucks')
@@ -11,10 +11,13 @@ nunjucks.configure('src/views',{
 })
 
 server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
 // Configuração de arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
 // Rotas da Aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 .listen(5500)
